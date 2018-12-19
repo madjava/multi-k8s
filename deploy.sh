cd39df75cde992a4a79bb644d506d20113998315
Felix Eyetan <felix.eyetan@thomsonreuters.com>
2018-12-19 16:52:41 +0000

    Added deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t madjava/multi-client:latest -t madjava/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t madjava/multi-server:latest -t madjava/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t madjava/multi-worker:latest -t madjava/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push madjava/multi-client:latest
+docker push madjava/multi-server:latest
+docker push madjava/multi-worker:latest
+
+docker push madjava/multi-client:$SHA
+docker push madjava/multi-server:$SHA
+docker push madjava/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployments server=madjava/multi-server:$SHA
+kubectl set image deployments/client-deployments client=madjava/multi-client:$SHA
+kubectl set image deployments/worker-deployments worker=madjava/multi-worker:$SHA
