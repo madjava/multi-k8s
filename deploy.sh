0c96624a5a3080e7c2387819189cd2f3f45d21ff
Felix Eyetan <peyetan@yahoo.com>
2020-11-14 16:06:25 +0000

    Adding more configuration

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,16 @@
-docker build -t madjava/multi-client -f ./client/Dockerfile ./client
-docker build -t madjava/multi-server -f ./server/Dockerfile ./server
-docker build -t madjava/multi-worker -f ./worker/Dockerfile ./worker
-docker push madjava/multi-client
-docker push madjava/multi-server
-docker push madjava/multi-worker
+docker build -t madjava/multi-client:latest -t madjava/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t madjava/multi-server:latest -t madjava/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t madjava/multi-worker:latest -t madjava/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push madjava/multi-client:latest
+docker push madjava/multi-server:latest
+docker push madjava/multi-worker:latest
+
+docker push madjava/multi-client:$SHA
+docker push madjava/multi-server:$SHA
+docker push madjava/multi-worker:$SHA
+
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=madjava/multi-server
+kubectl set image deployments/server-deployment server=madjava/multi-server:$SHA
+kubectl set image deployments/client-deployment client=madjava/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=madjava/multi-worker:$SHA
